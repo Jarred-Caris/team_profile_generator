9f106fd6b4078e9d7af93df13338d3a0db3a4441
jarred <jarred.caris@gmail.com>
2021-02-21 18:20:58 +1030

    created html and css templates, wrote tests for employee.js file

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,24 @@
+const Employee = require("./lib/Employee");
+
+test ("Can instantiate Employee instance", () => {
+    const employee = new Employee();
+    expect(typeof(employee)).toBe("object");
+});
+
+test ("Can set name through constructor", () => {
+    const name = "Jarred";
+    const employee = new Employee(name);
+    expect(employee.name).toBe(name);
+});
+
+test ("Can set ID through constructor", () => {
+    const number = 1234;
+    const employee = new Employee(number);
+    expect(employee.id).toBe(number);
+});
+
+test ("Can set email through constructor", () => {
+    const number = 1234;
+    const employee = new Employee(number);
+    expect(employee.id).toBe(number);
+});
--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-const Employee = require("./Employee");
-
+const Employee = require("./lib/Employee");
+const inquirer =require("inquirer")
 const employee = new Employee();
 
-employee.init();
+const survey =[
+    {
+        type: 'input',
+        message: 'What is the new employee name?',
+        name: 'name'
+    },
+    {
+        type: 'input',
+        message: 'What is the above employees id number?',
+        name: 'id'
+    },
+    {
+        type: 'input',
+        message: 'What is the above employees email address?',
+        name: 'email'
+    }
+]
+
+function init () {
+    inquirer.prompt(survey)
+    .then((answer) => console.log(answer))
+}
+init();
 
 
