8c25a31124a181e2315049c22f78253f640e7661
jarred <jarred.caris@gmail.com>
2021-02-26 13:36:15 +1030

    index.js completed

--- index.js
+++ index.js
@@ -1,52 +1,146 @@
-// const Employee = require("./lib/Employee");
-// const Manager = require("./lib/Manager");
-// const Engineer = require("./lib/Engineer");
-// const Intern = require("./lib/Intern");
-const inquirer =require("inquirer")
-// const employee = new Employee();
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 
+const employees = [];
 
-const questions =[
-    {
-        type: 'input',
-        message: 'What is the new employee name?',
-        name: 'name'
-    },
-    {
-        type: 'input',
-        message: 'What is the above employees id number?',
-        name: 'id'
-    },
-    {
-        type: 'input',
-        message: 'What is the above employees email address?',
-        name: 'email'
-    }
-]
+const managerQuestions = [
+  {
+    type: "input",
+    message: "What is the new employee name?",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "What is the above employees id number?",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "What is the above employees email address?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What is the Office ID Number",
+    name: "officeNumber",
+  },
+];
 
-// const managerQuestions =[
-//     {
-//         type: 'input',
-//         message: 'What is the Office ID Number',
-//         name: 'officeNumber'
-//     }]
+const engineerQuestions = [
+  {
+    type: "input",
+    message: "What is the new employee name?",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "What is the above employees id number?",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "What is the above employees email address?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "Please provide the Github address for this Engineer",
+    name: "github",
+  },
+];
 
-function start(){
-    inquirer
-    .prompt(questions)
-    .then(res=> {
-        console.log(res)
+const internQuestions = [
+  {
+    type: "input",
+    message: "What is the new employee name?",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "What is the above employees id number?",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "What is the above employees email address?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "Please provide the education institute this intern attends",
+    name: "school",
+  },
+];
+
+function start() {
+  inquirer
+    .prompt(managerQuestions)
+    .then((res) => {
+      console.log(res);
+      manager = new Manager(res.name, res.id, res.email, res.officeNumber);
+      employees.push("Manager");
+      createTeam();
     })
-    .catch()
-
+    .catch();
 }
 start();
 
-// function init () {
-//     inquirer.prompt(questions)
-//     .then((answer) => console.log(answer))
-// }
-// init();
+function createTeam() {
+  console.log("create Team");
+  inquirer
+    .prompt([
+      {
+        name: "choice",
+        type: "rawlist",
+        choices: ["Engineer", "Intern", "Exit"],
+        message: "What role would you like to submit?",
+      },
+    ])
+    .then((res) => {
+      console.log(res);
+      switch (res.choice) {
+        case "Engineer":
+          createEngineer();
+          break;
+        case "Intern":
+          createIntern();
+          break;
+        default:
+        generateHTML(res);
+      }
+    });
+}
+
+function createEngineer() {
+  inquirer.prompt(engineerQuestions).then((res) => {
+    console.log(res);
+    engineer = new Engineer(res.name, res.id, res.email, res.github);
+    employees.push("Engineer");
+    createTeam();
+  });
+}
+
+function createIntern() {
+    inquirer.prompt(internQuestions).then((res) => {
+      console.log(res);
+      intern = new Intern(res.name, res.id, res.email, res.school);
+      employees.push("Intern");
+      createTeam();
+    });
+  }
 
 
+ const generateHTML = () => {
+     console.log(employees);
+     const teamComplete = JSON.stringify(employees)
+     fs.writeFile('employees.json', teamComplete, function(err) {
+         if (err) throw (err);
+         console.log('Welcome to your new team!')
+     })
+ }
+
+ module.exports = generateHTML;
