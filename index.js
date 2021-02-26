d2f3266b06ad1e3fd94ceb6653df498ce484e1e1
jarred <jarred.caris@gmail.com>
2021-02-26 11:28:07 +1030

    completed employee questions and start function

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const Employee = require("./lib/Employee");
+// const Employee = require("./lib/Employee");
+// const Manager = require("./lib/Manager");
+// const Engineer = require("./lib/Engineer");
+// const Intern = require("./lib/Intern");
 const inquirer =require("inquirer")
-const employee = new Employee();
+// const employee = new Employee();
 
-const survey =[
+
+
+const questions =[
     {
         type: 'input',
         message: 'What is the new employee name?',
@@ -20,10 +25,28 @@ const survey =[
     }
 ]
 
-function init () {
-    inquirer.prompt(survey)
-    .then((answer) => console.log(answer))
+// const managerQuestions =[
+//     {
+//         type: 'input',
+//         message: 'What is the Office ID Number',
+//         name: 'officeNumber'
+//     }]
+
+function start(){
+    inquirer
+    .prompt(questions)
+    .then(res=> {
+        console.log(res)
+    })
+    .catch()
+
 }
-init();
+start();
+
+// function init () {
+//     inquirer.prompt(questions)
+//     .then((answer) => console.log(answer))
+// }
+// init();
 
 
