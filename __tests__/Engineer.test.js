98b6dfab71b58c5231d8bbd7251604bc7f391ac6
jarred <jarred.caris@gmail.com>
2021-02-26 16:24:22 +1030

    passed 7 tests, still have 3 fails

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -3,19 +3,19 @@ const Engineer = require("../lib/Engineer");
 describe("Engineer", () => {
   describe("Initialization", () => {
     it("should create a github username property", () => {
-      const engineer = new Engineer("Sarah", "1", "sarah@email", "hub");
+      const engineer = new Engineer("Sarah", "1", "sarah@email", "sarah");
 
-      expect(engineer.github).toEqual("hub");
+      expect(engineer.github).toEqual("sarah");
     });
 
-    it("should return a github account when getGithub() function is called", () => {
+    it("should return a gitsarah account when getGithub() function is called", () => {
         const engineer = new Engineer("Sarah", "1", "sarah@email", "sarah");
   
-        expect(engineer.getGithub()).toEqual("hub");
+        expect(engineer.getGithub()).toEqual("sarah");
       });
   
       it("should return all information when getRole() function is called", () => {
-        const engineer = new Engineer("Sarah", "1", "sarah@email", "hub");
+        const engineer = new Engineer("Sarah", "1", "sarah@email", "sarah");
         const test = "Engineer";
   
         expect(engineer.getRole()).toEqual(test);
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -5,13 +5,13 @@ describe("Intern", () => {
     it("should create a school property", () => {
       const intern = new Intern("Sarah", "1", "sarah@email", "Uni");
 
-      expect(intern.school).toEqual("uni");
+      expect(intern.school).toEqual("Uni");
     });
 
     it("should return a name when getSchool() function is called", () => {
         const intern = new Intern("Sarah", "1", "sarah@email", "Uni");
   
-        expect(intern.getSchool()).toEqual("uni");
+        expect(intern.getSchool()).toEqual("Uni");
       });
   
       it("should return all information when getRole() function is called", () => {
